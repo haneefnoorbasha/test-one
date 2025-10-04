5926da70a6768e6d903ecf294128f25281b08cf6
anilreddy224466 <anilreddy.kunda1@gmail.com>
2025-10-04 11:47:58 -0500

    Initial commit-setup related

---
+++ src/main/java/com/bny/se/gittest/TestOneApplication.java
@@ -0,0 +1,14 @@
+package com.bny.se.gittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestOneApplication.class, args);
+		System.out.println("---Application Started---");
+	}
+
+}
