648478664c7590ab209afee3d89b94ddbc25126b
Haneef <haneef.noorbasha@gmail.com>
2025-10-04 18:41:22 -0500

    Haneef New Comment1

--- src/main/java/com/bny/se/gittest/feature01/SalaryService.java
+++ src/main/java/com/bny/se/gittest/feature01/SalaryService.java
@@ -3,10 +3,10 @@ package com.bny.se.gittest.feature01;
 public class SalaryService {
     public void m1(){
         System.out.println("---M1 method---");
-        System.out.println("---M1 method new line---");
-
+        System.out.println("---M1-New method new line---");
     }
       public void m2(){
         System.out.println("---M2 method---");
+          
     }
 }
