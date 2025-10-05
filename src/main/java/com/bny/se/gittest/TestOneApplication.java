192a1a6d747db7abb8d366941025af9ce8e5bcc7
anilreddy224466 <anilreddy.kunda1@gmail.com>
2025-10-05 18:43:19 -0500

    Add Salary Service

--- src/main/java/com/bny/se/gittest/TestOneApplication.java
+++ src/main/java/com/bny/se/gittest/TestOneApplication.java
@@ -1,5 +1,6 @@
 package com.bny.se.gittest;
 
+import com.bny.se.gittest.dto.EmployeeDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,10 @@ public class TestOneApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestOneApplication.class, args);
 		System.out.println("---Application Started---");
+
+		EmployeeDto dto = new EmployeeDto();
+		dto.setName("Test");
+		System.out.println(dto.getName());
 	}
 
 }
